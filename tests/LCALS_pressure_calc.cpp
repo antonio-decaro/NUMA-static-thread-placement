98bb5da2f07400e7163a87bd68a29a74fab3497b
Antonio De Caro <antonio.decaro99@outlook.it>
2022-06-22 18:54:01 +0000

    Modified benchmar configuration

--- tests/LCALS_pressure_calc.cpp
+++ tests/LCALS_pressure_calc.cpp
@@ -56,9 +56,11 @@ int main() {
   auto t1 = high_resolution_clock::now();
 #pragma omp parallel firstprivate(cls, p_cut, pmin, eosvmax)
   {
-    for (int i = 0; i < 5; i++) {
-      initData(loopData[i], i, len);
-    }
+    initData(loopData[0], 0, len);
+    initData(loopData[1], 1, len);
+    initData(loopData[2], 2, len);
+    initData(loopData[3], 3, len);
+    initData(loopData[4], 4, len);
 
     double* compression = loopData[0];
     double* bvc = loopData[1];
@@ -99,6 +101,6 @@ int main() {
     counting_stop(evset);
     counting_fini(evset);
   }
-  
+
   cout << (t2-t1).count() << '\n';
 }
